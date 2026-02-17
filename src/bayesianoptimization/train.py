4b1538a8df9755644fc133b9f07d374d3d8e0f83
PB-02 <s246103@dtu.dk>
2026-02-17 15:30:12 +0100

    added minimizers

--- src/bayesianoptimization/train.py
+++ src/bayesianoptimization/train.py
@@ -1,6 +1,7 @@
-from bayesianoptimization.model import Model
+from bayesianoptimization.model import CNNModel
 from bayesianoptimization.data import MyDataset
 from skopt.space import Integer, Real
+from skopt import gp_minimize, dummy_minimize
 
 
 search_space = [
@@ -9,11 +10,48 @@ search_space = [
     Real(0.0, 0.1, 0.3, 0.5, name = "dropout_rate")
 ]
 
+def objective(params):
+    depth, kernel_size, dropout_rate = params
+
+    model = CNNModel(
+        depth=depth,
+        kernel_size=kernel_size,
+        dropout_rate=dropout_rate
+    )
+
+    validation_score = "INDSÆT TRÆNINGSFUNKTIONEN HER"
+
+    return validation_score
+
+results_ei = gp_minimize(
+    func=objective,
+    dimensions=search_space,
+    n_calls=20,
+    n_random_starts=5,
+    acq_func='EI', 
+    random_state=42
+)
+
+results_pi = gp_minimize(
+    func=objective,
+    dimensions=search_space,
+    n_calls=20,
+    n_random_starts=5,
+    acq_func='PI', 
+    random_state=42
+)
+
+results_random = dummy_minimize(
+    func=objective,
+    dimensions=search_space,
+    n_calls=20,
+    random_state=42
+)
 
 
 def train():
     dataset = MyDataset("data/raw")
-    model = Model()
+    model = CNNModel()
     # add rest of your training code here
 
 if __name__ == "__main__":
