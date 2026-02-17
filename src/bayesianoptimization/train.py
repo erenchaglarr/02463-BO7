ea83d57ff092ef7bc1c9e2c0eb068ecdf71f074f
erenchaglarr <s245735@dtu.dk>
2026-02-17 14:01:07 +0100

    initial commit

---
+++ src/bayesianoptimization/train.py
@@ -0,0 +1,10 @@
+from bayesianoptimization.model import Model
+from bayesianoptimization.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
