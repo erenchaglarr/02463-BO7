0b4ecef0abe1e7a49c1d8777b7c1757e7a23b033
PB-02 <s246103@dtu.dk>
2026-02-17 14:54:00 +0100

    added search space

--- src/bayesianoptimization/train.py
+++ src/bayesianoptimization/train.py
@@ -1,5 +1,15 @@
 from bayesianoptimization.model import Model
 from bayesianoptimization.data import MyDataset
+from skopt.space import Integer, Real
+
+
+search_space = [
+    Integer(2, 5, 10, 20, name = 'depth'),
+    Integer(3, 5, 7, 9, name = "kernel_size"),
+    Real(0.0, 0.1, 0.3, 0.5, name = "dropout_rate")
+]
+
+
 
 def train():
     dataset = MyDataset("data/raw")
