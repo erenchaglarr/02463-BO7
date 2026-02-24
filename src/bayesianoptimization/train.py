514efb9232ae8d23897d769d430564e0e80766c4
erenchaglarr <s245735@dtu.dk>
2026-02-24 17:12:22 +0100

    added cuda line on train

--- src/bayesianoptimization/train.py
+++ src/bayesianoptimization/train.py
@@ -26,7 +26,11 @@ except ImportError:
 torch.manual_seed(42)
 np.random.seed(42)
 
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+device = torch.device(
+    "cuda" if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available()
+    else "cpu"
+)
 print(f"Using device: {device}")
 
 
